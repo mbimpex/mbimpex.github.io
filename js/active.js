dbd33bccbfb288ba61c226f58ce3f063d1ab1458
mbimpex <46795019+mbimpex@users.noreply.github.com>
2019-10-22 23:01:59 +0200

    Revert "zajeb"
    This reverts commit 27aeee2e5f5c023cade696fc70339c05ac2f7e9f.

--- js/active.js
+++ js/active.js
@@ -12,7 +12,7 @@
 
     // :: 2.0 Nav Active Code
     if ($.fn.classyNav) {
-        $('#alazeaNav').classyNav();
+        $('#mbimpexNav').classyNav();
     }
 
     // :: 3.0 Search Active Code
@@ -70,7 +70,7 @@
 
     // :: 5.0 Masonary Gallery Active Code
     if ($.fn.imagesLoaded) {
-        $('.alazea-portfolio').imagesLoaded(function () {
+        $('.mbimpex-portfolio').imagesLoaded(function () {
             // filter items on button click
             $('.portfolio-filter').on('click', 'button', function () {
                 var filterValue = $(this).attr('data-filter');
@@ -79,7 +79,7 @@
                 });
             });
             // init Isotope
-            var $grid = $('.alazea-portfolio').isotope({
+            var $grid = $('.mbimpex-portfolio').isotope({
                 itemSelector: '.single_portfolio_item',
                 percentPosition: true,
                 masonry: {
@@ -148,7 +148,7 @@
 
     // :: 10.0 Sticky Active Code
     if ($.fn.sticky) {
-        $(".alazea-main-menu").sticky({
+        $(".mbimpex-main-menu").sticky({
             topSpacing: 0
         });
     }
